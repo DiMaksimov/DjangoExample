a242cdda03f316724cd58930b74f9fc5b3ac2d18
DM <maksymov.d.a@gmail.com>
2019-03-25 00:38:20 +0200

    Part 1 - 4.5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -12,15 +13,26 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "polls/index.html", context)
 
 
-def detail(request, question_id):
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
-def results(request: HttpRequest, question_id):
-    response = f"You're looking at the results of question {question_id}"
-    return HttpResponse(response)
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
-def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
